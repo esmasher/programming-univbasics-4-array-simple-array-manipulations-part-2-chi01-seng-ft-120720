aa243c57e49f1f15ade8fcd4949ba6cbb992e2dd
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 23:13:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,28 @@ using_concat(my_favorite_things, more_favs)
   more_favs = ["mario kart", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat(more_favs)
 end
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+  @another_esoteric_language = "Malbolge"
+  @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
+end
+
+def using_uniq(captain_planet_and_the_planeteers)
+  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
+  new_array = captain_planet_and_the_planeteers.uniq
+end
+
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  flat_array = private_colleges_in_newyork.flatten
+end
+
+def using_delete(my_list, my_element)
+  my_list.delete(my_element)
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+  integer = 2
+  deleted_robot = famous_robots.delete_at(integer)
+end
