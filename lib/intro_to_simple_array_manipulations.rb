728b4534a7b48a14cfbc0ebe8b3d0fecd6b90356
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 23:15:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ using_concat(my_favorite_things, more_favs)
 end
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  @another_esoteric_language = "Malbolge"
+  @another_esoteric_language = "Python"
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
 
